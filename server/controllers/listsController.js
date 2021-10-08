0df245a667f0b282531d0ba44d7f273cf1b8f32c
Tzvi-H <16guitar@gmail.com>
2021-10-08 13:38:19 -0500

    implemented controller for updating list (updateList)
    Co-authored-by: Caleb caleb.heath@gmail.com
    
    Co-authored-by: Tzvi 16guitar@gmail.com

--- server/controllers/listsController.js
+++ server/controllers/listsController.js
@@ -18,24 +18,26 @@ const addListToBoard = (req, res, next) => {
   Board.findByIdAndUpdate(boardId, {
     $addToSet: { lists: list._id }, // adds list to the lists array in board
   }).then(() => {
+    res.status(201)
     next();
   });
 };
 
 const sendList = (req, res, next) => {
-  res.status(201).json(req.list)
+  // res.status(201).json(req.list)
+  res.json(req.list)
 }
 
 const createList = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
-    // Board.findById(req.body.boardId)
-    //   .then(board => {
-    //     if (!board) {
-    //       return res.status(404).json({
-    //         message: "board id doesn't exist"
-    //       })
-    //     } 
+    Board.findById(req.body.boardId)
+      .then(board => {
+        if (!board) {
+          return res.status(404).json({
+            message: "board id doesn't exist"
+          })
+        }
         
         const newList = {
           title: req.body.list.title,
@@ -46,13 +48,13 @@ const createList = (req, res, next) => {
           req.list = list
           next()
         }).catch(e => console.log(e))
-      // })
-      // .catch(e => {
-      //   console.log(e)
-      //   res.status(404).json({
-      //     message: "board id is invalid"
-      //   })
-      // })
+      })
+      .catch(e => {
+        console.log(e)
+        res.status(404).json({
+          message: "board id is invalid"
+        })
+      })
 
   } else {
     return next(new HttpError("The input field is empty.", 422));
@@ -62,14 +64,31 @@ const createList = (req, res, next) => {
 const updateList = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
-    res.json({msg: "success"})
+    const updatedProperties = {};
+    if (req.body.title) { updatedProperties.title = req.body.title };
+    if (req.body.position) { updatedProperties.position = req.body.position };
+    List
+      .findByIdAndUpdate(req.params.id, updatedProperties,  { new: true })
+      .then(list => {
+        if (!list) {
+          return res.status(404).json({
+            message: "list doesn't exist"
+          })
+        } 
+        res.status(200)
+        req.list = list
+        next()
+      })
+      .catch(e => {
+        console.log(e)
+        res.status(404).json({msg: "invalid id"})
+      })
   } else {
-    res.json({msg: "failed validation"})
+    return next(new HttpError("The input field is empty.", 422));
   }
 }
 
 exports.createList = createList;
-
 exports.sendList = sendList;
 exports.addListToBoard = addListToBoard;
 exports.updateList = updateList
